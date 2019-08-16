e7bd95ab6004b8793421e36377a07a84f5bde2b4
Carlos Gonzalez <cagl18@gmail.com>
2019-08-16 13:27:27 -0400

    Updates

---
+++ precache-manifest.c438f1458cf3c24bd67e06709f655970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03447a9aaf794d4d89cd865b0ac4b248",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "9b2f579e058e93e9a778",
+    "url": "/burger-builder/static/css/main.b5119a15.chunk.css"
+  },
+  {
+    "revision": "76268ec41a053ee74f39",
+    "url": "/burger-builder/static/js/2.cfa301a5.chunk.js"
+  },
+  {
+    "revision": "9b2f579e058e93e9a778",
+    "url": "/burger-builder/static/js/main.6b260ce4.chunk.js"
+  },
+  {
+    "revision": "3e4d375b0fabb6673ac4",
+    "url": "/burger-builder/static/js/runtime~main.be0250f8.js"
+  }
+]);
